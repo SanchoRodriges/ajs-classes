cb17817f7a64c350fe757a547b99a3d101d17475
Sancho Rodriges <softilla@ya.ru>
2022-12-20 09:21:41 +0300

    methods #4

--- src/js/__tests__/undead.test.js
+++ src/js/__tests__/undead.test.js
@@ -2,10 +2,9 @@ import Undead from '../class/Undead';
 
 test('Правильно создаётся объект', () => {
   const undead = new Undead('Undead');
-    const correct = {
-      attack: 25, defence: 25, health: 100, level: 1, name: 'Undead', type: 'Undead',
-    };
-  
-    expect(undead).toEqual(correct);
-  });
-  
+  const correct = {
+    attack: 25, defence: 25, health: 100, level: 1, name: 'Undead', type: 'Undead',
+  };
+
+  expect(undead).toEqual(correct);
+});
